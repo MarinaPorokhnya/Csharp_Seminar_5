a1f5bf6f17696b6609628dce101f9c72e0bbdb6c
MarinaPorokhnya <marina.vbyaby.poroxnya@mail.ru>
2024-07-05 11:32:03 +0300

    Добавили решение задача1с

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+// Пример
+// 2 3 4 3      4 3 16 3
+//  4 3 4 1 =>  4 3 4 1
+//  2 9 5 4     4 9 25 4
+
+void Main()
+{
+    int rowCount = ReadInt("Введите количество строк двумерного массива: ");
+    int colCount = ReadInt("Введите количество столбцов двумерного массива: ");
+
+    int[,] matrix = GenerateMatrix(rowCount, colCount, -99, 99);
+    PrintMatrix(matrix);
+}
+
+void PrintMatrix(int[,] matrixForPrint)
+{
+    for (int i = 0; i < matrixForPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixForPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixForPrint[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+    System.Console.WriteLine();
+}
+
+int[,] GenerateMatrix(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] tempMatrix = new int[rows, cols];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(minValue, maxValue + 1);
+        }
+    }
+
+    return tempMatrix;
+}
+
+
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
